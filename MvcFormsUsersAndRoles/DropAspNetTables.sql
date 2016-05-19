a444660b2b77c6c8703c7c58957e8f6de0577b21
Jack Stephens <jack@jrcs3.com>
2016-05-18 18:29:14 -0700

    Add starter project

---
+++ MvcFormsUsersAndRoles/DropAspNetTables.sql
@@ -0,0 +1,28 @@
+﻿USE MvcFormsUsersAndRoles
+GO
+BEGIN TRANSACTION;
+BEGIN TRY
+	DROP TABLE AspNetUserRoles
+	DROP TABLE AspNetUserClaims
+	DROP TABLE AspNetUserLogins
+	DROP TABLE AspNetRoles
+	DROP TABLE AspNetUsers
+	DROP TABLE __MigrationHistory
+	RAISERROR('Made it to here!',16,1);
+END TRY
+BEGIN CATCH
+    SELECT 
+         ERROR_NUMBER() AS ErrorNumber
+        ,ERROR_SEVERITY() AS ErrorSeverity
+        ,ERROR_STATE() AS ErrorState
+        ,ERROR_PROCEDURE() AS ErrorProcedure
+        ,ERROR_LINE() AS ErrorLine
+        ,ERROR_MESSAGE() AS ErrorMessage;
+
+    IF @@TRANCOUNT > 0
+        ROLLBACK TRANSACTION;
+END CATCH;
+
+IF @@TRANCOUNT > 0
+    COMMIT TRANSACTION;
+GO
